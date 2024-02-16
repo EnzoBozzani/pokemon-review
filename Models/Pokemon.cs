fc057da40a5431f29dd88cff232c0c78d4cd4703
Enzo Bozzani <bozzanienzo@gmail.com>
2024-02-16 19:01:01 -0300

    feat: created models

---
+++ Models/Pokemon.cs
@@ -0,0 +1,9 @@
+namespace PokemonReview.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Middleware config below
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
